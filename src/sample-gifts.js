ffad79daf23373995b521288f1860354e6a1a07d
thisispayam <thisispayam@gmail.com>
2017-07-25 15:24:38 -0400

    starting empty template

---
+++ src/sample-gifts.js
@@ -0,0 +1,74 @@
+// sample data!
+module.exports = {
+  gift1: {
+    name: 'T-shirt and Jeans combo',
+    image: 'https://images.pexels.com/photos/346745/pexels-photo-346745.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'T-shirt and Jeans',
+    price: 7525,
+    status: 'available'
+  },
+
+  gift2: {
+    name: 'Jacket',
+    image: 'https://images.pexels.com/photos/291762/pexels-photo-291762.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Red Jacket for you',
+    price: 9000,
+    status: 'available'
+  },
+
+  gift3: {
+    name: 'Stripe Dress',
+    image: 'https://images.pexels.com/photos/206470/pexels-photo-206470.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Unique Stripe dress',
+    price: 3525,
+    status: 'unavailable'
+  },
+
+  gift4: {
+    name: 'Designer combo',
+    image: 'https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Designer combo package',
+    price: 9950,
+    status: 'available'
+  },
+
+  gift5: {
+    name: 'Red heels',
+    image: 'https://images.pexels.com/photos/336372/pexels-photo-336372.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Red high heels for a great taste.',
+    price: 8775,
+    status: 'available'
+  },
+
+  gift6: {
+    name: 'Classic shoes',
+    image: 'https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Classic shoes made with love!',
+    price: 8850,
+    status: 'available'
+  },
+
+  gift7: {
+    name: 'Baby hat',
+    image: 'https://images.pexels.com/photos/101537/baby-boy-hat-covered-101537.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Baby hat for a good looking baby.',
+    price: 3500,
+    status: 'available'
+  },
+
+  gift8: {
+    name: 'Designer watch',
+    image: 'https://images.pexels.com/photos/508677/pexels-photo-508677.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'A watch to remember',
+    price: 9025,
+    status: 'available'
+  },
+
+  gift9: {
+    name: 'Custom cap',
+    image: 'https://images.pexels.com/photos/206462/pexels-photo-206462.jpeg?h=350&auto=compress&cs=tinysrgb',
+    desc: 'Choose your text and wear it',
+    price: 2575,
+    status: 'available'
+  }
+};
