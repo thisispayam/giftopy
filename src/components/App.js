c5ed1ff5c71db3f159de394f8baa15d302951604
thisispayam <thisispayam@gmail.com>
2017-08-01 16:48:53 -0400

    Added the add to order button logic

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component{
         //binding our methods 'this' to the App
         this.addGift = this.addGift.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
+        this.addToOrder = this.addToOrder.bind(this);
         //getInitialState
         this.state = {
             gifts:{},
@@ -32,16 +33,24 @@ class App extends React.Component{
             gifts: sampleGifts
         });
     }
+    addToOrder(key){
+        //take a copy of our state
+        const order = {...this.state.order};
+        //update/add the new number of gifts ordered
+        order[key] = order[key] + 1 || 1;
+        // update our state
+        this.setState({order});
+    }
     render(){
         return(
             <div className="giftopy">
                 <div className="menu">
-                    <Header tagline="Gift for your loved ones"/>
+                    <Header tagline="Vacation deals and promotions"/>
                     <ul className="list-of-gifts">
                         {
                             Object
                                 .keys(this.state.gifts)
-                                .map(key => <Gift key={key} details={this.state.gifts[key]} />) 
+                                .map(key => <Gift key={key} index={key} details={this.state.gifts[key]} addToOrder = {this.addToOrder} />) 
                         }
                     </ul>
                 </div>
